5acfc884faea591aa5ed926027f993233bb5aeb9
Jason <63689065+jgarner9@users.noreply.github.com>
2024-04-10 10:48:35 -0600

    added mockDb to authenticate controller and improved mock credential flow

--- client/src/controllers/authenticate.jsx
+++ client/src/controllers/authenticate.jsx
@@ -1,6 +1,25 @@
 export default function authenticate(e) {
   e.preventDefault();
-  const [username, password] = e.target.children;
-  localStorage.setItem("user", `${username.value},${password.value}`);
-  location.reload();
+
+  const mockDb = {
+    username: "jgarner",
+    password: "peanuts",
+  };
+
+  //destructure form event children, to get values
+  const [username, password] = [e.target.username.value, e.target.password.value];
+
+  console.log(e.target.username.value)
+  console.log(e.target.password.value)
+
+  //logic to check mock credentials
+  if (
+    username === mockDb.username &&
+    password === mockDb.password
+  ) {
+    localStorage.setItem("user", `${username.value},${password.value}`);
+    location.reload();
+  } else {
+    document.getElementById("invalid-login-error").hidden = false;
+  }
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,23 @@
-import authenticate from "../controllers/authenticate"
+import authenticate from "../controllers/authenticate";
 import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div id="login-page-container">
+      <h1 id="invalid-login-error" hidden>
+        Invalid Login
+      </h1>
       <h1>Login</h1>
-      <form id="login-form" onSubmit={(e) => {authenticate(e)}}>
-        <input type="text" id="username-input" required />
-        <input type="password" id="password-input" required />
+      <form
+        id="login-form"
+        onSubmit={(e) => {
+          authenticate(e);
+        }}
+      >
+        <label>Username:</label>
+        <input type="text" id="username-input" name="username" required />
+        <label>Password:</label>
+        <input type="password" id="password-input" name="password" required />
         <input type="submit" id="login-button" value="Login" />
       </form>
       <Link to="/signup">Sign Up</Link>
