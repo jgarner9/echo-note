ea3efdb2e23a829d026acbe08b18547df2bb02b4
Jason <63689065+jgarner9@users.noreply.github.com>
2024-04-19 23:41:16 -0600

    removed comments

--- server/routers/authRouter.mjs
+++ server/routers/authRouter.mjs
@@ -11,9 +11,7 @@ import verifyWebToken from "../controllers/verifyWebToken.mjs";
 authRouter.use((req, res, next) => verifyWebToken(req, res, next));
 
 authRouter.put("/change-username", (req, res) => changeUsername(req, res));
-//TODO: change password route
 authRouter.put("/change-password", (req, res) => changePassword(req, res));
-//TODO: delete account
 authRouter.delete("/delete-account", (req, res) => deleteAccount(req, res));
 
 export { authRouter };
